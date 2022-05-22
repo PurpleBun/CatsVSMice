a98a42e00aaabb3d4a6cb2a8ca49b89e66eb95d7
Phuong Nguyen <haphuong311202@gmail.com>
2022-05-22 21:57:46 +0200

    Win condition + Update states
    Win condition to game manager
    Search for now has angle limitation
    CatAI inherit basic stats from cats collision doesnt work for some reasons so still local
    Colliding with cat removes mouse + interact with winning condition

--- Assets/Scripts/CatAI/CatActor/PhuongCat.cs
+++ Assets/Scripts/CatAI/CatActor/PhuongCat.cs
@@ -6,7 +6,8 @@ using UnityEngine.AI;
 namespace CatAI
 {
     [RequireComponent(typeof(NavMeshAgent))]
-    public class Cat : MonoBehaviour
+    //inherit from CatActor
+    public class PhuongCat :  CatActor  
     {
         private StateMachine stateMachine = new StateMachine();
         [SerializeField]
@@ -20,6 +21,10 @@ namespace CatAI
         void Start()
         {
             navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+            //setting up navmesh agent with inherited stats
+            navMeshAgent.speed = baseSpeed;
+            navMeshAgent.angularSpeed = angularSpeed;
+            navMeshAgent.acceleration = acceleration;
             stateMachine.ChangeState(new SearchFor(this.mouseLayer, this.gameObject, this.viewRange, this.mouseTag, MiceFound));
         }
 
@@ -32,6 +37,15 @@ namespace CatAI
         {
             var foundmice = searchResults.AllHitObjectsWithRequiredTag;
             stateMachine.ChangeState(new Move(this.navMeshAgent,foundmice[0]));
+        }
+        
+        void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag == "Mouse") {
+                //remove mouse from game manager list to check for winning condition
+                manager.mouseList.Remove(other.gameObject);
+                Destroy(other.gameObject);
+            }
         }
     }
 }
