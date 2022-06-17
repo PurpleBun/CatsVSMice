7287553e42723ca8261e840121d213b0bd4bbc56
Robert-Smeman <robertsmeman@hotmail.com>
2022-06-17 23:52:07 +0200

    Added ReadMe document for the teachers just in case about what scenes we use for the game.
    The scenes that we use for the game are Main Menu and Furniture.

---
+++ Assets/TEACHERS README.cs
@@ -0,0 +1,11 @@
+//
+//
+//
+//
+//
+//The scenes that we use for the game are Main Menu and Furniture.
+//The scenes that we use for the game are Main Menu and Furniture.
+//The scenes that we use for the game are Main Menu and Furniture.
+//The scenes that we use for the game are Main Menu and Furniture.
+//The scenes that we use for the game are Main Menu and Furniture.
+//The scenes that we use for the game are Main Menu and Furniture.
