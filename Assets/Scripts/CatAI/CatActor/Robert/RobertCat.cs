7d9c473535f5ac49ac15804d80c8eafe90f2e549
Robert-Smeman <robertsmeman@hotmail.com>
2022-06-17 23:41:01 +0200

    Updated my cat AI
    Added comments to my AI

--- Assets/Scripts/CatAI/CatActor/Robert/RobertCat.cs
+++ Assets/Scripts/CatAI/CatActor/Robert/RobertCat.cs
@@ -44,6 +44,7 @@ namespace CatAI
             stateMachine.ExecuteStateUpdate();
         }
 
+        //Random location on map
         public void Wander()
         {
             stateMachine.ChangeState(new Wander(navMeshAgent, this.gameObject, stateMachine));
@@ -54,13 +55,13 @@ namespace CatAI
             stateMachine.ChangeState(new SearchAll(this.gameObject, this.viewRange, FindMouseAndTrap));
         }
 
+        //Search for mouse and trap.
         public void FindMouseAndTrap(AllSearchResults searchResultsMouseAndTrap)
         {
             var findMouse = searchResultsMouseAndTrap.AllMice;
-            //var findHole = searchResults.AllHole;
             var findTrap = searchResultsMouseAndTrap.AllTrap;
-            //var findCat = searchResults.AllCat;
 
+            //If no mouse found and no trap found, go wander
             if(findMouse.Count == 0) 
             {
                 if(findTrap.Count == 0) 
@@ -69,6 +70,7 @@ namespace CatAI
                     Wander();
                     return;
                 }
+                //If no mouse found but there is a trap, go check if the trap has been set already
                 else if (findTrap.Count != 0)
                 {
                     CheckTrap();
@@ -77,12 +79,14 @@ namespace CatAI
                 Wander();
                 return;
             }
+            //If there is a mouse, go check if there's a cat and hole nearby
             else if (findMouse.Count != 0)
             {
                 SearchCatAndHole();
             }
         }
 
+        //Check if hte trap has been set, if set already, go wander
         public void CheckTrap()
         {
             if(TrapStateManager.TrapActivated == true)
@@ -91,12 +95,14 @@ namespace CatAI
                 stateMachine.ChangeState(new Wander(navMeshAgent, this.gameObject, stateMachine));
                 return;
             }
+            //If trap not yet set, go set trap
             else if (TrapStateManager.TrapActivated == false)
             {
                 stateMachine.ChangeState(new SearchFor(this.gameObject, this.viewRange, this.trapTag, SetTrap));
             }
         }
 
+        //Setting the trap, move closer if out of reach, set trap
         public void SetTrap(SearchResults searchResultsMouseAndTrap)
         {
             var foundtrap = searchResultsMouseAndTrap.AllHitObjectsWithRequiredTag;
@@ -111,17 +117,18 @@ namespace CatAI
             }
         }
 
+        // Search for a cat and hole
         public void SearchCatAndHole()
         {
-            stateMachine.ChangeState(new SearchAll(this.gameObject, this.viewRange, FindCatAndMouse));
+            stateMachine.ChangeState(new SearchAll(this.gameObject, this.viewRange, FindCatAndHole));
         }
 
-        public void FindCatAndMouse(AllSearchResults searchResultsCatAndHole)
+        public void FindCatAndHole(AllSearchResults searchResultsCatAndHole)
         {
             var findCat = searchResultsCatAndHole.AllCat;
             var findHole = searchResultsCatAndHole.AllHole;
             
-
+            //If ther is no cat and no hole, go chase the mouse
             if(findCat.Count == 0) 
             {
                 if(findHole.Count == 0)
@@ -129,18 +136,23 @@ namespace CatAI
                     // no cat or hole found, chase the found mouse.
                     stateMachine.ChangeState(new SearchAll(this.gameObject, this.viewRange, Chase));
                 }
+                //If there is no cat but there is a hole
                 else if(findHole.Count != 0)
                 {
                     //Move between hidey hole and mouse. Then chase
+                    stateMachine.ChangeState(new SearchAll(this.gameObject, this.viewRange, Chase));
 
                 }
             }
+            //If there is a cat but no hidey hole
             else if(findCat.Count != 0)
             {
                 if(findHole.Count == 0)
                 {
                     //move behind mouse, pin mouse in between cats. Then chase.
+                    stateMachine.ChangeState(new SearchAll(this.gameObject, this.viewRange, Chase));
                 }
+                //If there is a cat and a hidey hole, move infront of hidey hole, then go chase
                 else if(findHole.Count != 0)
                 {
                     //Move infront of hidey hole. Then chase.
@@ -153,17 +165,18 @@ namespace CatAI
             }
         }
 
+        //Chase the mouse
         public void Chase(AllSearchResults searchResultsCatAndHole)
         {
             var findMouse = searchResultsCatAndHole.AllMice;
             stateMachine.ChangeState(new Move(this.navMeshAgent, findMouse[0].transform.position));
         }
 
+        //On collision with mouse, remoe mouse from list, deactivate it and shake screen
         void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.tag == "Mouse") {
                 Shake();
-                //remove mouse from game manager list to check for winning condition
                 manager.mouseList.Remove(other.gameObject);
                 other.gameObject.SetActive(false);
             }
